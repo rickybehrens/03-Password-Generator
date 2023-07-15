945f2728f788dbdd404429141669a245d7244df5
Borito <behrensricardo@gmail.com>
2023-07-15 14:00:51 -0600

    Defined variables, prompts and functions. Based on the user's selection, I created an array that give me the information to creat the password. Still haven't printed out the password

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,41 +35,112 @@ var alphabet = "abcdefghijklmnopqrstuvwxyz"
 var alphaLower = alphabet.split('')
 var alphaUpper = alphabet.toUpperCase().split('')
 var special = " !#$%&’()*+,-./:;<=>?@[\]^_`{|}~"
-var espCha = special.split('')
-var passRange = {
-    from: 8,
-    to: 128,
-}
+var speCha = special.split('')
+var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-var passwordCriteria = {
-    lenght: 8,
-    lowercase: true,
-    uppercase: true,
-    numeric: true,
-    specialCharacters: false
-}
+
+
 
 // Functions
 
 // Prompt for length
-var lenght = prompt("Choose a password length between 8 and 128 charaters")
-console.log(lenght)
-
 // Validate length
-if (8 > lenght > 128) {
-    alert("Password's length needs to be between 8 and 128 characters")
-}
-
-// Prompt for characters
-// Validate characters
-// Generate password
-// Show password
-
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var length = prompt("Choose a password length between 8 and 128 characters?");
+    if (length < 8 || length > 128 || isNaN(length)) {
+        alert("Password must be between 8 and 128 characters")
+        return
+    } else {
+        console.log("Password length is: " + length + " characters")
+    };
 
-  passwordText.value = password;
+    // Prompt for characters
+    var lowercase = confirm("Would you like to include lowercase charaters?");
+    if (lowercase) {
+        console.log("You have chose to include lowercase characters in your password " + lowercase);
+    }
+
+    // Prompt for characters
+    var uppercase = confirm("Would you like to include uppercase charaters?");
+    if (uppercase) {
+        console.log("You have chose to include uppercase characters in your password " + uppercase);
+    }
+
+    // Prompt for characters
+    var specialCharacters = confirm("Would you like to include special charaters?");
+    if (specialCharacters) {
+        console.log("You have chose to include special characters in your password " + specialCharacters);
+    }
+
+    // Prompt for characters
+    var numeric = confirm("Would you like to include numbers?");
+    if (numeric) {
+        console.log("You have chose to include numbers in your password " + numeric);
+    }
+
+    var userChoice = [
+        length,
+        lowercase,
+        uppercase,
+        specialCharacters,
+        numeric,
+    ]
+    console.log(userChoice)
+    console.log(userChoice[0])
+
+    if (lowercase) {
+        var array1 = alphaLower
+    }
+
+    if (uppercase) {
+        var array2 = alphaUpper
+    }
+
+    if (specialCharacters) {
+        var array3 = speCha
+    }
+
+    if (numeric) {
+        var array4 = num
+    }
+
+    if (lowercase === false && uppercase === false && specialCharacters === false && numeric === false) {
+        
+    }
+
+    for (let index = 0; index < userChoice[0]; index++) {
+        console.log(randArray[Math.floor(Math.random() * randArray.length)])
+        alert("You must choose at least one type of characters for your password")
+        return
+    }
+
+    var randArray = array1.concat(array2, array3, array4)
+    console.log(randArray)
+
+
+
+    /*
+    * - If the user selects a length that doesn't meet the criteria, there should be an alert and it should rederict him to the length prompt again
+    * - If the user selects the correct length, the next prompt should come up
+    * - After selecting lowercase characters, the next prompt should come up
+    * - After selecting uppercase charaters, the next prompt shold come up
+    * - After selecting special characters, the next rpompt should come up
+    * - After selecting numbers, the password should be generated
+    * - Show password
+    * - Ask if we need to generate a new password
+    */
+
+
+
+
+    // Validate characters
+    // Generate password
+    // Show password
+
+    // var password = generatePassword();
+    // var passwordText = document.querySelector("#password");
+
+    // passwordText.value = password;
 
 }
 
